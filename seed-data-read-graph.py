d4477c104e36f37029f1240318a4a8970eb2f3b5
cgarbin <garbin.christian@gmail.com>
2018-08-25 15:20:42 -0400

    Graph data

--- seed-data-read-graph.py
+++ seed-data-read-graph.py
@@ -48,6 +48,35 @@ def main():
     df_high = df.loc[df['high salary'] == 'yes']
     print(df_high)
 
+    # Simple graph, using specific columns
+    df.plot(x='first name', y='salary', kind='barh', legend=False)
+    # Note: the graph won't display at this point
+    # See call to plt.show() below
+
+    # Choosing colors for the bars, based on another column
+    df.plot(x='first name', y='salary', kind='barh', legend=False,
+            color=np.where(df['high salary'] == 'yes', 'r', 'b'))
+
+    # Same as above, using list comprehension
+    df.plot(x='first name', y='salary', kind='barh', legend=False, color=[
+            'r' if x == 'yes' else 'b' for x in df['high salary']])
+
+    # Using categorical data for colors
+    # While possible in pandas/matlibplot, this is easier with seaborn
+    import seaborn as sns
+    sns.pairplot(x_vars=['hire date'], y_vars=['salary'],
+                 data=df, hue='education level', height=5)
+    # Use matlibplot to control the legend (seaborn is based on matlibplot)
+    # Without this line the legend is cut off (displayed to the right, not visible)
+    plt.legend(loc=0)
+
+    # Display all graphs at once
+    # If we call this after each .plot, it will show the graph and interrupt
+    # the program until that graph is window is closed
+    # Note that in some cases the graph windows may be displayed right on top
+    # of each other - move them around to see all graphs
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
