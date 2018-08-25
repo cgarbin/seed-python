bdf1bbb6420b68eea9f062ca1a4b3b4bebfae67f
cgarbin <garbin.christian@gmail.com>
2018-08-25 15:19:51 -0400

    Add column based on data in other columns

--- seed-data-read-graph.py
+++ seed-data-read-graph.py
@@ -24,6 +24,24 @@ def main():
     df['high salary'] = np.where(df['salary'] >= 7000, 'yes', 'no')
     print(df)
 
+    # Add a new column based on the value of another column
+    # List comprehension version
+    # See https://stackoverflow.com/q/50375985/336802 for comparison of methods
+    df['high salary'] = ['yes' if x >= 7000 else 'no' for x in df['salary']]
+    print(df)
+
+    # Add a column using a function - for more involved cases
+    def vacation_days(hire_date):
+        today = pd.to_datetime('today')
+        service_years = (today - hire_date) / np.timedelta64(1, 'Y')
+        if service_years >= 20:
+            return 25
+        if service_years >= 15:
+            return 20
+        return 10
+    df['vacation days'] = df['hire date'].apply(vacation_days)
+    print(df[['first name', 'vacation days']])
+
 
 if __name__ == '__main__':
     main()
